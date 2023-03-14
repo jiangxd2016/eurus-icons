6e0de94eb5e127ea57895ebc48a7cd081bbc5fe1
jiangxd <jiangxd2016@gmail.com>
2023-03-14 16:50:43 +0800

    fix: fix icon name

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import ElIcons from 'eurus-icons-vue/global';
+import EuIcons from 'eurus-icons-vue/global';
 import App from './App.vue';
 
-createApp(App).use(ElIcons).mount('#app');
+createApp(App).use(EuIcons).mount('#app');
